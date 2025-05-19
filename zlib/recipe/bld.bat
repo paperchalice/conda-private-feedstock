1eb2451a79d61cb4286157887429e3b424a34e15
paperchalice <liujunchang97@outlook.com>
2025-05-19 16:36:56 +0800

    debug

--- zlib/recipe/bld.bat
+++ zlib/recipe/bld.bat
@@ -2,7 +2,7 @@ cmake -G "NMake Makefiles" -S . -B build ^
     -DCMAKE_BUILD_TYPE=MinSizeRel ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DWITH_GTEST=OFF ^
-    -DZLIB_COMPAT=ON
+    -DZLIB_COMPAT=ON ^
     -DBUILD_SHARED_LIBS=ON
 cmake --build build
 cmake --install build
