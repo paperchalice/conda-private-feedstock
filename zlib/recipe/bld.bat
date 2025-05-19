eb57ae40d5404994d8d4de3297dbdef6781cc9b6
paperchalice <liujunchang97@outlook.com>
2025-05-19 16:19:05 +0800

    use zlib

--- zlib/recipe/bld.bat
+++ zlib/recipe/bld.bat
@@ -2,6 +2,7 @@ cmake -G "NMake Makefiles" -S . -B build ^
     -DCMAKE_BUILD_TYPE=MinSizeRel ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DWITH_GTEST=OFF ^
-    -DBUILD_SHARED_LIBS=ON ^
+    -DZLIB_COMPAT=ON
+    -DBUILD_SHARED_LIBS=ON
 cmake --build build
 cmake --install build
