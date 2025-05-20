e786b793c55ee4321b7fa91b37c3b61367830ac6
paperchalice <liujunchang97@outlook.com>
2025-05-20 10:55:13 +0800

    zlib: refine

--- zlib/recipe/bld.bat
+++ zlib/recipe/bld.bat
@@ -1,7 +1,8 @@
 cmake -S . -B build  ^
     %CMAKE_ARGS%     ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DWITH_GTEST=OFF ^
     -DZLIB_COMPAT=ON ^
-    -DBUILD_SHARED_LIBS=ON
+    -DINSTALL_UTILS=ON
 cmake --build build
 cmake --install build
