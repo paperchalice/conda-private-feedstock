6e104346dc6ce9f93d6be8c68a5820eb01a5174a
paperchalice <liujunchang97@outlook.com>
2025-05-30 17:04:57 +0800

    update

--- vc/recipe/VsLatest/VsLatest.psm1
+++ vc/recipe/VsLatest/VsLatest.psm1
@@ -3,7 +3,7 @@ $Env:CC = "cl.exe"
 $Env:CXX = "cl.exe"
 
 $vswhere = vswhere -format json -latest -utf8 | ConvertFrom-Json
-$vs_ver_major = $vswhere.catalog_productLine -replace 'Dev'
+$vs_ver_major = $vswhere.catalog.productLine -replace 'Dev'
 
 Import-Module "$($vswhere.installationPath)\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell $vswhere.instanceId -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
@@ -11,7 +11,10 @@ Enter-VsDevShell $vswhere.instanceId -SkipAutomaticLocation -DevCmdArguments "-a
 # CMake environments
 if ($Env:CONDA_BUILD) {
     $Env:CMAKE_BUILD_TYPE = 'MinSizeRel'
-    $Env:CMAKE_GENERATOR = "Visual Studio $vs_ver_major $($vswhere.productLineVersion)"
+    $Env:CMAKE_GENERATOR = "Visual Studio $vs_ver_major $($vswhere.catalog.productLineVersion)"
     $Env:CMAKE_GENERATOR_PLATFORM = 'x64'
     $Env:CMAKE_INSTALL_PREFIX = $LIBRARY_PREFIX
+
+    $CMAKE_BUILD_TYPE = $Env:CMAKE_BUILD_TYPE
+    Export-ModuleMember -Variable "CMAKE_BUILD_TYPE"
 }
