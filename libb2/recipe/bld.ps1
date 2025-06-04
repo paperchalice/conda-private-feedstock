7635629695139b99c4e00beff37c468c1dec1e2b
paperchalice <liujunchang97@outlook.com>
2025-06-04 17:11:15 +0800

    fix pkgconfig

--- libb2/recipe/bld.ps1
+++ libb2/recipe/bld.ps1
@@ -11,3 +11,5 @@ Rename-Item -Path "$LIBRARY_LIB\b2.dll.lib" -NewName "b2.lib"
 $data = Get-Content "$LIBRARY_LIB\libb2.la"
 $data = $data.Replace('b2.dll.lib', 'b2.lib')
 $data | Out-File -encoding ASCII "$LIBRARY_LIB\libb2.la"
+$data = Get-Content "$LIBRARY_LIB\pkgconfig\libb2.pc"
+$data -replace '^prefix=.+', "prefix=$LIBRARY_PREFIX" | Out-File -encoding ASCII "$LIBRARY_LIB\pkgconfig\libb2.pc"
