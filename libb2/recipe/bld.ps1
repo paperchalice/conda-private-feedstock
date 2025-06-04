ab35b8c7b8f46778d7799e152ae05256661a117d
paperchalice <liujunchang97@outlook.com>
2025-06-04 16:42:38 +0800

    libb2

---
+++ libb2/recipe/bld.ps1
@@ -0,0 +1,13 @@
+$Env:AR = (Get-Command x86_64-w64-mingw32-ar).Path -replace '\\', '/' -replace 'C:', '/c/'
+$Env:CFLAGS = ($Env:CFLAGS -replace '/', '-') + '-O1'
+$MSYS2_PREFIX = $LIBRARY_PREFIX -replace '\\', '/' -replace 'C:', '/c/'
+
+bash -c './autogen.sh'
+bash -c "./configure --disable-static --prefix=$(cygpath $MSYS2_PREFIX)"
+bash -c 'make'
+bash -c 'make install'
+
+Rename-Item -Path "$LIBRARY_LIB\b2.dll.lib" -NewName "b2.lib"
+$data = Get-Content "$LIBRARY_LIB\libb2.la"
+$data = $data.Replace('b2.dll.lib', 'b2.lib')
+$data | Out-File -encoding ASCII "$LIBRARY_LIB\libb2.la"
