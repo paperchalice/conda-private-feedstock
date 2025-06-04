96468e773fd6329624cb470a5327b1ab8ce52c4a
paperchalice <liujunchang97@outlook.com>
2025-06-04 12:55:56 +0800

    update

--- lz4/recipe/bld.ps1
+++ lz4/recipe/bld.ps1
@@ -3,3 +3,7 @@ cmake -S build/cmake -B build `
     -DINSTALL_UTILS=ON
 cmake --build build --config $CMAKE_BUILD_TYPE
 cmake --install build --config $CMAKE_BUILD_TYPE
+
+foreach ($i in @('lz4cat', 'unlz4')) {
+    New-Item -Path "$LIBRARY_BIN\$i.exe" -ItemType SymbolicLink 'lz4.exe' 
+}
