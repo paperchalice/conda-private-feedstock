36e4a27ffef6d894f368c54a7ae0de240f72e232
paperchalice <liujunchang97@outlook.com>
2025-06-07 17:33:28 +0800

    binutils

---
+++ binutils/recipe/bld.ps1
@@ -0,0 +1,9 @@
+$tools = @('add2line', 'ar', 'as', 'c++filt', 'dlltool', 'dllwrap', 'elfedit', 'gprof',
+  'ld.bfd', 'ld', 'nm', 'objcopy', 'objdump', 'ranlib', 'readelf', 'size', 'strings', 'strip',
+  'windmc', 'windres')
+
+$tgt = 'x86_64-w64-mingw32-'
+
+foreach ($t in $tools) {
+  New-Item -Path "$LIBRARY_BIN\$t.exe" -ItemType SymbolicLink -Target "$tgt$t.exe"
+}
