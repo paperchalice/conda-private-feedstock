ac1bde1476ac107f4fe0dc21f465778bf74c10ff
paperchalice <liujunchang97@outlook.com>
2025-06-11 14:00:57 +0800

    debug

--- m2-aux/recipe/bld.ps1
+++ m2-aux/recipe/bld.ps1
@@ -1,5 +1,7 @@
 $tools = @('windres-rc', 'cl_cpp_wrapper', 'compile', 'ar-lib')
 
+$dest = "$LIBRARY_PREFIX\usr\bin"
+New-Item -Path $dest -ItemType Directory -Force
 foreach ($t in $tools) {
-    Copy-Item "$RECIPE_DIR\$t" -Destination "$LIBRARY_PREFIX\usr\bin"
+    Copy-Item "$RECIPE_DIR\$t" -Destination $dest
 }
