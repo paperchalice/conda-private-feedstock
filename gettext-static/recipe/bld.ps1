1493c7ce94537703d67556eac1375eb47c37d825
paperchalice <liujunchang97@outlook.com>
2025-06-15 20:44:23 +0800

    use array

--- gettext-static/recipe/bld.ps1
+++ gettext-static/recipe/bld.ps1
@@ -1,5 +1,11 @@
 Enter-M2
 $MSYS2_PREFIX = $LIBRARY_PREFIX -replace '\\', '/' -replace 'C:', '/c/'
-bash -c "./configure --prefix=$MSYS2_PREFIX --disable-nls --enable-shared=no --enable-static=yes --build=x86_64-w64-mingw32"
+$config_args = @("--prefix=$MSYS2_PREFIX",
+  '--disable-nls',
+  '--enable-shared=no',
+  '--enable-static=yes',
+  '--build=x86_64-w64-mingw32'
+) -join ' '
+bash -c "./configure $config_args"
 bash -c 'make'
 bash -c 'make install'
