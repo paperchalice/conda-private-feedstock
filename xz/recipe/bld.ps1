afdf1a9a57cd774ea8571e106b1b195681f9b065
paperchalice <liujunchang97@outlook.com>
2025-06-15 16:22:48 +0800

    symlinks

--- xz/recipe/bld.ps1
+++ xz/recipe/bld.ps1
@@ -5,3 +5,8 @@ cmake -S . -B build `
   -DBUILD_SHARED_LIBS=ON
 cmake --build build --config $CMAKE_BUILD_TYPE
 cmake --install build --config $CMAKE_BUILD_TYPE
+
+$xz_tools = @('unxz', 'xzcat', 'lzma', 'unlzma', 'lzcat')
+foreach ($t in $xz_tools) {
+  New-Item -Path "$LIBRARY_BIN\$t.exe" -ItemType SymbolicLink -Target "xz.exe"
+}
