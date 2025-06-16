edeced9faa4f8d6d37f854d735dfd69fe5bda11e
paperchalice <liujunchang97@outlook.com>
2025-06-16 09:09:06 +0800

    disable

--- gettext-static/recipe/bld.ps1
+++ gettext-static/recipe/bld.ps1
@@ -4,8 +4,17 @@ $config_args = @("--prefix=$MSYS2_PREFIX",
   '--disable-nls',
   '--enable-shared=no',
   '--enable-static=yes',
+  '--disable-csharp',
+  '--disable-java',
+  '--disable-nls',
   '--build=x86_64-w64-mingw32'
 ) -join ' '
 bash -c "./configure $config_args"
 bash -c 'make -i'
 bash -c 'make install -i'
+
+Remove-Item -Recurse $LIBRARY_INC $LIBRARY_LIB
+$shares = @('doc', 'info', 'man')
+foreach ($d in $shares) {
+  Remove-Item -Recurse $LIBRARY_PREFIX\share\$d
+}
