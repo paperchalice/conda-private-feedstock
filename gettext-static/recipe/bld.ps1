45e7423f2985f804aade9ff3b9719f7531ed8312
paperchalice <liujunchang97@outlook.com>
2025-06-17 12:27:19 +0800

    gettext binary

--- gettext-static/recipe/bld.ps1
+++ gettext-static/recipe/bld.ps1
@@ -1,25 +1,8 @@
-$hello = 'libtextstyle\adhoc-tests\hello.c'
-(Get-Content $hello).Replace('int i;', 'int i; void*Ptr = &sprintf;') | `
-  Set-Content $hello
-
-Enter-M2
-$prefix = $LIBRARY_PREFIX.Replace('\', '/')
-$Env:RC = 'windres-rc rc'
-$config_args = @("--prefix=$prefix",
-  '--disable-nls',
-  '--enable-shared=no',
-  '--enable-static=yes',
-  '--disable-csharp',
-  '--disable-java',
-  '--enable-relocatable',
-  '--build=x86_64-w64-mingw32'
-) -join ' '
-bash -c "./configure $config_args"
-bash -c 'make'
-bash -c 'make install'
-
-Remove-Item -Recurse $LIBRARY_INC $LIBRARY_LIB
-$shares = @('doc', 'info', 'man')
-foreach ($d in $shares) {
-  Remove-Item -Recurse $LIBRARY_PREFIX\share\$d
+Remove-Item bin\iconv.exe
+Remove-Item share\doc -Recurse
+$dirs = @(
+  'bin', 'libexec', 'share'
+)
+foreach($d in $dirs) {
+  Copy-Item -Path $d -Destination $LIBRARY_PREFIX -Recurse
 }
