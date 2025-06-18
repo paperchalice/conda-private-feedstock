206b659a0b5686289370d2ebe2932f76b5751ef8
PaperChalice <liujunchang97@outlook.com>
2025-06-18 16:50:28 +0800

    gettext

--- gettext-static/recipe/bld.ps1
+++ gettext-static/recipe/bld.ps1
@@ -4,5 +4,8 @@ $dirs = @(
   'bin', 'libexec', 'share'
 )
 foreach($d in $dirs) {
+  if (Test-Path $LIBRARY_PREFIX\$d) {
+    Remove-Item -Recurse -Force $LIBRARY_PREFIX\$d 
+  }
   Copy-Item -Path $d -Destination $LIBRARY_PREFIX -Recurse
 }
