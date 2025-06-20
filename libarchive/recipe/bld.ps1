da54de60cd2d5ef4eb18e68892aab672a2fa6a8b
PaperChalice <liujunchang97@outlook.com>
2025-06-20 17:47:12 +0800

    fix

--- libarchive/recipe/bld.ps1
+++ libarchive/recipe/bld.ps1
@@ -1,7 +1,9 @@
-cmake -S . -B build `
+cmake -S . -B build  --debug-find `
     -DBUILD_SHARED_LIBS=ON `
     -DENABLE_TAR_SHARED=ON `
     -DENABLE_CPIO_SHARED=ON `
-    -DENABLE_CAT_SHARED=ON
+    -DENABLE_CAT_SHARED=ON `
+    -DOPENSSL_INCLUDE_DIR=$Env:BUILD_PREFIX\Library\include `
+    -DOPENSSL_CRYPTO_LIBRARY=$Env:BUILD_PREFIX\Library\lib\crypto.lib
 cmake --build build --config $CMAKE_BUILD_TYPE
 cmake --install build --config $CMAKE_BUILD_TYPE
