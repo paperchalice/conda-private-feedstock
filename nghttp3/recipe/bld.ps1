faf663db26193b037b836f55c4341bbfecb413cc
paperchalice <liujunchang97@outlook.com>
2025-06-22 21:21:06 +0800

    nghttp3

--- nghttp3/recipe/bld.ps1
+++ nghttp3/recipe/bld.ps1
@@ -1,10 +1,6 @@
-$Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
-
 $cmake_args = @(
-  '-DENABLE_ARES=ON',
-  '-DCURL_USE_LIBSSH=ON',
-  '-DUSE_WIN32_IDN=ON',
-  '-DUSE_OPENSSL_QUIC=ON'
+  '-DENABLE_LIB_ONLY=ON',
+  '-DENABLE_STATIC_LIB=OFF'
 )
 
 cmake -S . -B build -DBUILD_SHARED_LIBS=ON @cmake_args
