1a82264c28db64b10c777de7cb41f13a387b2dfa
PaperChalice <liujunchang97@outlook.com>
2025-06-23 16:01:31 +0800

    curl

--- curl/recipe/bld.ps1
+++ curl/recipe/bld.ps1
@@ -1,9 +1,12 @@
 $Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
-
+(Get-Content lib/CMakeLists.txt).Replace('"_imp"','""') | `
+  Set-Content lib/CMakeLists.txt
 $cmake_args = @(
   '-DENABLE_ARES=ON',
   '-DCURL_USE_LIBSSH=ON',
   '-DUSE_WIN32_IDN=ON',
+  '-DCURL_USE_OPENSSL=ON',
+  '-DLIBCURL_OUTPUT_NAME=curl'
   '-DUSE_OPENSSL_QUIC=ON'
 )
 
