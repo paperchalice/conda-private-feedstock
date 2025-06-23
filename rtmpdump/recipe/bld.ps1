6b538c1d1bec215ecaad745af0abddd3771071e8
PaperChalice <liujunchang97@outlook.com>
2025-06-23 15:47:58 +0800

    no rtmp

--- rtmpdump/recipe/bld.ps1
+++
@@ -1,9 +0,0 @@
-$Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
-
-Copy-Item "$RECIPE_DIR\CMakeLists.txt" -Destination .
-(Get-Content librtmp\rtmp_sys.h).Replace('#define snprintf', '//').Replace('#define vsnprintf', '//').Replace('#define EWOULDBLOCK', '//') | `
-  Set-Content librtmp\rtmp_sys.h
-
-cmake -S . -B build -DBUILD_SHARED_LIBS=ON
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
