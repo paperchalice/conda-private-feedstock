684154da3c09306a2046d45f40f3bdeb76351967
PaperChalice <liujunchang97@outlook.com>
2025-11-05 20:39:11 +0800

    vc

--- vc/recipe/VsLatest/VsLatest.psm1
+++ vc/recipe/VsLatest/VsLatest.psm1
@@ -7,34 +7,34 @@ $vswhere = vswhere -format json -latest -utf8 | ConvertFrom-Json
 $vs_ver_major = $vswhere.catalog.productLine -replace 'Dev'
 
 Import-Module "$($vswhere.installationPath)\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell $vswhere.instanceId -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
+Enter-VsDevShell $vswhere.instanceId -SkipAutomaticLocation -DevCmdArguments '-arch=x64 -host_arch=x64'
 
-# CMake environments
 if ($Env:CONDA_BUILD) {
+    # building packages
     [string[]]$cflag_list = @(
-        ,'-nologo'
-        ,'-MP'
-        ,'-Zc:inline'
-        ,'-Zc:preprocessor'
-        ,'-utf-8'
-        ,'-DWIN32'
-        ,'-D_WINDOWS'
-        ,'-DNDEBUG'
-        ,'-I', $($Env:LIBRARY_INC.Replace('\', '/'))
+        , '-nologo'
+        , '-MP'
+        , '-Zc:inline'
+        , '-Zc:preprocessor'
+        , '-utf-8'
+        , '-DWIN32'
+        , '-D_WINDOWS'
+        , '-DNDEBUG'
+        , '-I', $($Env:LIBRARY_INC.Replace('\', '/'))
     )
     [string[]]$cxxflag_list = $cflag_list + @(
-        ,'-permissive-'
-        ,'-Zc:__cplusplus'
-        ,'-Zc:checkGwOdr'
-        ,'-Zc:externConstexpr'
+        , '-permissive-'
+        , '-Zc:__cplusplus'
+        , '-Zc:checkGwOdr'
+        , '-Zc:externConstexpr'
         , '-Zc:referenceBinding'
         , '-Zc:rvalueCast',
         , '-Zc:templateScope'
     )
     [string[]]$ldflags_list = @(
-        ,'-NOLOGO'
-        ,'-INCREMENTAL:NO'
-        ,  "-LIBPATH:$($Env:LIBRARY_LIB.Replace('\', '/'))"
+        , '-NOLOGO'
+        , '-INCREMENTAL:NO'
+        , "-LIBPATH:$($Env:LIBRARY_LIB.Replace('\', '/'))"
     )
     $Env:CMAKE_BUILD_TYPE = 'MinSizeRel'
     $Env:CMAKE_GENERATOR = "Visual Studio $vs_ver_major $($vswhere.catalog.productLineVersion)"
@@ -49,6 +49,11 @@ if ($Env:CONDA_BUILD) {
 
     $CMAKE_BUILD_TYPE = $Env:CMAKE_BUILD_TYPE
     Export-ModuleMember -Variable "CMAKE_BUILD_TYPE"
+} else {
+    # normal environment
+    $Env:INCLUDE="$Env:CONDA_PREFIX\Library\include;$Env:INCLUDE"
+    $Env:LIB="$Env:CONDA_PREFIX\Library\lib;$Env:LIB"
+    $Env:CMAKE_PREFIX_PATH="$Env:CONDA_PREFIX\Library;$Env:CMAKE_PREFIX_PATH"
 }
 
 function Enter-M2 {
