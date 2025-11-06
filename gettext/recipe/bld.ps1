d4b86b26f72fa5ddc21cfdd99929cc09486fd8d7
PaperChalice <liujunchang97@outlook.com>
2025-11-06 21:08:11 +0800

    dbg

--- gettext/recipe/bld.ps1
+++ gettext/recipe/bld.ps1
@@ -1,4 +1,11 @@
 Enter-M2
+
+$BashSrc = (Get-Command bash).Source
+if ($BashSrc -ne "$BUILD_PREFIX\Library\usr\bin\bash.exe")
+{
+    false
+}
+
 $MSYS2_PREFIX = $LIBRARY_PREFIX -replace '\\', '/' -replace 'C:', '/c/'
 bash -c "./configure --prefix=$MSYS2_PREFIX --enable-shared=yes --enable-static=no --build=x86_64-w64-mingw32"
 bash -c 'make'
