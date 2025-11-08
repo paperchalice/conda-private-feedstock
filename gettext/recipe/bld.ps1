491754cd0e9389ed5798755588b5ec0f89fab306
paperchalice <liujunchang97@outlook.com>
2025-11-08 10:00:45 +0800

    unistring

--- gettext/recipe/bld.ps1
+++ gettext/recipe/bld.ps1
@@ -4,11 +4,13 @@ Enter-M2
     , "--prefix=$(cygpath $LIBRARY_PREFIX)"
     , '--enable-shared=yes'
     , '--enable-static=no'
+    , '--enable-year2038'
     , '--build=x86_64-w64-mingw32'
     , "--with-libxml2-prefix=$(cygpath $LIBRARY_PREFIX)"
     , "--with-libiconv-prefix=$(cygpath $LIBRARY_PREFIX)"
+    , "--with-libunistring-prefix=$(cygpath $LIBRARY_PREFIX)"
 )
 
 bash -c "./configure $($configure_args -join ' ')"
-bash -c 'make'
+bash -c 'make -j'
 bash -c 'make install'
