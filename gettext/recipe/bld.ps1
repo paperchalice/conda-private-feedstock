23388a431ccc9870dae8eae080d8250a88198769
paperchalice <liujunchang97@outlook.com>
2025-11-08 14:09:31 +0800

    split

--- gettext/recipe/bld.ps1
+++ gettext/recipe/bld.ps1
@@ -5,12 +5,18 @@ Enter-M2
     , '--enable-shared=yes'
     , '--enable-static=no'
     , '--enable-year2038'
+    , '--enable-relocatable'
     , '--build=x86_64-w64-mingw32'
     , "--with-libxml2-prefix=$(cygpath $LIBRARY_PREFIX)"
     , "--with-libiconv-prefix=$(cygpath $LIBRARY_PREFIX)"
     , "--with-libunistring-prefix=$(cygpath $LIBRARY_PREFIX)"
+    , '--disable-csharp'
+    , '--disable-java'
 )
 
 bash -c "./configure $($configure_args -join ' ')"
 bash -c 'make -j'
 bash -c 'make install'
+
+Get-ChildItem $LIBRARY_LIB\*.dll.lib | Rename-Item -NewName { $_.Name -replace '\.dll.lib','.lib' }
+Remove-Item $LIBRARY_PREFIX\share\gettext\archive.dir.tar.xz
