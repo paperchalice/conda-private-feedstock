9c5a7f64bdb6ad9489df751ef9c61bf53dae4dc2
PaperChalice <liujunchang97@outlook.com>
2025-11-10 21:15:56 +0800

    new iconv

--- iconv/recipe/bld.ps1
+++
@@ -1,9 +0,0 @@
-Enter-M2
-$Env:RC = 'windres-rc rc'
-$prefix = $LIBRARY_PREFIX.Replace('\', '/')
-bash -c "./configure --prefix=$prefix --enable-shared=yes --enable-relocatable --enable-static=no --enable-nls --host=x86_64-pc-windows"
-bash -c 'make'
-bash -c 'make install'
-
-Rename-Item $LIBRARY_LIB\charset.dll.lib $LIBRARY_LIB\charset.lib
-Rename-Item $LIBRARY_LIB\iconv.dll.lib $LIBRARY_LIB\iconv.lib
---
+++ libiconv/recipe/bld.ps1
@@ -0,0 +1,18 @@
+Enter-M2
+
+[string[]]$configure_args = @(
+    , "--prefix=$(cygpath $LIBRARY_PREFIX)"
+    , '--enable-shared=yes'
+    , '--enable-static=no'
+    , '--enable-year2038'
+    , '--enable-relocatable'
+    , '--enable-nls'
+    , '--build=x86_64-w64-mingw32'
+)
+
+bash -c "./configure $($configure_args -join ' ')"
+bash -c 'make -j'
+bash -c 'make install'
+
+Get-ChildItem $LIBRARY_LIB\*.dll.lib | Rename-Item -NewName { $_.Name -replace '\.dll.lib','.lib' }
+
