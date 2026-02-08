dd4596d1807c762e04218b22bb1b5745f1c4f52f
paperchalice <liujunchang97@outlook.com>
2026-02-08 20:06:35 +0800

    zlib (#9)

--- zlib/bld.ps1
+++ zlib/bld.ps1
@@ -3,5 +3,5 @@ cmake -S . -B build `
     -DWITH_GTEST=OFF `
     -DZLIB_COMPAT=ON `
     -DINSTALL_UTILS=ON
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
+cmake --build build
+cmake --install build
