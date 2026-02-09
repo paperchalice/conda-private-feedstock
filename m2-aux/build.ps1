0b1158ffd15fd9611ad2b74d3a9650af45da5af2
PaperChalice <liujunchang97@outlook.com>
2026-02-09 13:03:48 +0800

    just download

--- m2-aux/build.ps1
+++ m2-aux/build.ps1
@@ -1,6 +1,11 @@
-$tools = @('windres-rc', 'cl_cpp_wrapper', 'compile', 'ar-lib')
+$urls = @{
+    'ar-lib' = 'https://github.com/autotools-mirror/automake/raw/refs/heads/master/lib/ar-lib'
+    'compile' = 'https://github.com/autotools-mirror/automake/raw/refs/heads/master/lib/compile'
+    'cl_cpp_wrapper' = 'https://github.com/microsoft/vcpkg/raw/refs/heads/master/ports/vcpkg-make/wrappers/cl_cpp_wrapper'
+    'windres-rc' = 'https://github.com/microsoft/vcpkg/raw/refs/heads/master/ports/vcpkg-make/wrappers/windres-rc'
+}
 
 New-Item -Path $LIBRARY_BIN -ItemType Directory -Force
-foreach ($t in $tools) {
-    Copy-Item "$RECIPE_DIR\$t" -Destination $LIBRARY_BIN
+foreach ($entry in $urls.GetEnumerator()) {
+    Invoke-WebRequest $($entry.Value) -OutFile $LIBRARY_BIN\$($entry.Key)
 }
