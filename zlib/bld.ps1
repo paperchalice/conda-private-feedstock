8d15640621fb67522f0f403bf524e1290c6ee4d9
PaperChalice <liujunchang97@outlook.com>
2026-02-26 14:49:20 +0800

    zlib

--- zlib/bld.ps1
+++ zlib/bld.ps1
@@ -1,3 +1,6 @@
+$vswhere = vswhere -format json -latest -utf8 | ConvertFrom-Json
+& "$($vswhere.installationPath)\Common7\Tools\Launch-VsDevShell.ps1" -SkipAutomaticLocation -Arch amd64 -HostArch amd64
+
 cmake -S . -B build `
     -DBUILD_SHARED_LIBS=ON `
     -DWITH_GTEST=OFF `
