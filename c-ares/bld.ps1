1bfeb7215b745b2470e4a482e3629ca52ab5f9e2
PaperChalice <liujunchang97@outlook.com>
2026-03-02 21:12:59 +0800

    c-ares

--- c-ares/bld.ps1
+++ c-ares/bld.ps1
@@ -1,3 +1,3 @@
 cmake -S . -B build -DBUILD_SHARED_LIBS=ON
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
+cmake --build build
+cmake --install build
