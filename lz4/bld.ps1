51dfe84b05e8e6d180646e01e9bee0e681cae346
paperchalice <liujunchang97@outlook.com>
2026-03-02 21:16:00 +0800

    lz4 (#20)

--- lz4/bld.ps1
+++ lz4/bld.ps1
@@ -1,8 +1,8 @@
 cmake -S build/cmake -B build `
     -DBUILD_SHARED_LIBS=ON `
     -DINSTALL_UTILS=ON
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
+cmake --build build
+cmake --install build
 
 foreach ($i in @('lz4cat', 'unlz4')) {
     New-Item -Path "$LIBRARY_BIN\$i.exe" -ItemType SymbolicLink -Target 'lz4.exe' 
