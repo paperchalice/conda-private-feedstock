4ebbe4c1a160c09e2554a14db65826ff0fe546d6
paperchalice <liujunchang97@outlook.com>
2026-03-05 16:13:49 +0800

    brotli (#29)

--- brotli/bld.ps1
+++ brotli/bld.ps1
@@ -1,4 +1,4 @@
 cmake -S . -B build `
     -DBUILD_SHARED_LIBS=ON
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
+cmake --build build
+cmake --install build
