b3c905cbd04377f2df7ccd0bee8a868a666107ab
PaperChalice <liujunchang97@outlook.com>
2026-03-05 20:44:57 +0800

    libarchive

--- libarchive/bld.ps1
+++ libarchive/bld.ps1
@@ -1,14 +1,14 @@
-$Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
 # remove BUILD_SHARED_LIBS=ON in next release
 cmake -S . -B bld `
     -DCMAKE_INSTALL_LIBDIR=lib `
     -DBUILD_SHARED_LIBS=ON `
     -DENABLE_TEST=OFF `
+    -DENABLE_LZO=ON `
     -DENABLE_TAR_SHARED=ON `
     -DENABLE_CPIO_SHARED=ON `
     -DENABLE_CAT_SHARED=ON
-cmake --build bld --config $CMAKE_BUILD_TYPE
-cmake --install bld --config $CMAKE_BUILD_TYPE
+cmake --build bld
+cmake --install bld
 
 Remove-Item $LIBRARY_LIB\archive_static.lib
 foreach ($i in @('cat', 'cpio', 'tar')) {
