a668b2c5e9402ad4410419e6ca1bbd310a41b9c7
paperchalice <liujunchang97@outlook.com>
2026-03-06 20:53:16 +0800

    krb5 (#32)

--- krb5/bld.ps1
+++ krb5/bld.ps1
@@ -1,10 +1,10 @@
 Set-Location src
-$Env:OPENSSL_DIR = "$Env:BUILD_PREFIX\Library"
+$Env:OPENSSL_DIR = $LIBRARY_PREFIX
 $Env:OPENSSL_VERSION = 3
 $Env:Path += ";$Env:WindowsSdkVerBinPath\x86"
 $Env:KRB_INSTALL_DIR=$LIBRARY_PREFIX
-(Get-Content config\win-pre.in).Replace('CFLAGS = $(CCOPTS)', "CFLAGS = `$(CCOPTS) $Env:CFLAGS /O1").Replace('libcrypto', 'crypto') | `
-  Set-Content config\win-pre.in
+$Env:MY_CFLAGS = $CFLAGS
+
 nmake -f Makefile.in prep-windows
 nmake NODEBUG=1
 nmake install NODEBUG=1
