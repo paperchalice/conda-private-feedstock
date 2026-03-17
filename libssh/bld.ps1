310b4044bbba86587c8f4e92ac26ab1e41af228f
paperchalice <liujunchang97@outlook.com>
2026-03-17 14:28:28 +0800

    libssh (#35)

--- libssh/bld.ps1
+++ libssh/bld.ps1
@@ -1,17 +1,14 @@
-$Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
+$Env:OPENSSL_ROOT_DIR = "$PREFIX\Library"
 (Get-Content src/gssapi.c).Replace('#include <gssapi/gssapi.h>',
   "#define WIN32_LEAN_AND_MEAN`n#include <gssapi/gssapi.h>") | `
   Set-Content src/gssapi.c
 (Get-Content src/libcrypto.c).Replace('EVP_CIPHER_CTX_init', 'EVP_CIPHER_CTX_reset') | `
   Set-Content src/libcrypto.c
 
-Remove-Item $Env:BUILD_PREFIX\Library\lib\libcrypto.lib
-
 $cmake_options = @(
   '-DBUILD_SHARED_LIBS=ON',
   '-DWITH_EXAMPLES=OFF',
-  "-DGSSAPI_ROOT_DIR=$Env:BUILD_PREFIX\Library",
-  "-DOPENSSL_CRYPTO_LIBRARY=$Env:BUILD_PREFIX\Library\lib\crypto.lib",
+  "-DGSSAPI_ROOT_DIR=$PREFIX\Library",
   '-DGSSAPI_FLAVOR_MIT=ON'
   '-DGSSAPI_LIBRARIES=gssapi64.lib;krb5_64.lib;comerr64.lib'
 )
