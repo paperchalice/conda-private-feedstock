66252554dc1ff1e7added8dd5bf84231cfa2772b
PaperChalice <liujunchang97@outlook.com>
2026-03-17 16:45:54 +0800

    curl

--- curl/bld.ps1
+++ curl/bld.ps1
@@ -1,15 +1,16 @@
-$Env:OPENSSL_ROOT_DIR = "$Env:BUILD_PREFIX\Library"
-(Get-Content lib/CMakeLists.txt).Replace('"_imp"','""') | `
-  Set-Content lib/CMakeLists.txt
 $cmake_args = @(
   '-DENABLE_ARES=ON',
   '-DCURL_USE_LIBSSH=ON',
   '-DUSE_WIN32_IDN=ON',
+  '-DUSE_NGTCP2=ON',
+  '-DUSE_LIBRTMP=ON',
+  '-DENABLE_UNICODE=ON',
+  '-DCURL_WINDOWS_SSPI=ON'
   '-DCURL_USE_OPENSSL=ON',
   '-DLIBCURL_OUTPUT_NAME=curl'
   '-DUSE_OPENSSL_QUIC=ON'
 )
-
+$Env:CMAKE_PREFIX_PATH = $LIBRARY_PREFIX
 cmake -S . -B build -DBUILD_SHARED_LIBS=ON @cmake_args
-cmake --build build --config $CMAKE_BUILD_TYPE
-cmake --install build --config $CMAKE_BUILD_TYPE
+cmake --build build
+cmake --install build
