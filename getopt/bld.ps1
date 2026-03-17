5a4344953fd577df4262e1e0a07dccce50e5371f
paperchalice <liujunchang97@outlook.com>
2026-03-17 18:19:45 +0800

    getopt (#40)

--- getopt/bld.ps1
+++ getopt/bld.ps1
@@ -2,5 +2,5 @@ cmake -S . -B bld `
     -DBUILD_SHARED_LIBS=ON `
     -DBUILD_STATIC_LIBS=OFF `
     -DBUILD_TESTING=OFF
-cmake --build bld --config $CMAKE_BUILD_TYPE
-cmake --install bld --config $CMAKE_BUILD_TYPE
+cmake --build bld
+cmake --install bld
