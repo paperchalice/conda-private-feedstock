efd6f5726a754d97136e7ed61f6b2c55f5832e32
PaperChalice <liujunchang97@outlook.com>
2026-07-31 21:29:56 +0800

    bump

--- nghttp2/build.ps1
+++ nghttp2/build.ps1
@@ -1,8 +1,9 @@
 $cmake_args = @(
   '-DENABLE_LIB_ONLY=ON',
+  '-DBUILD_SHARED_LIBS=ON',
   '-DENABLE_STATIC_LIB=OFF'
 )
 
-cmake -S . -B build -DBUILD_SHARED_LIBS=ON @cmake_args
+cmake -S . -B build @cmake_args
 cmake --build build
 cmake --install build
