60834808e20657c33e21ccaee664a0a1263b686b
PaperChalice <liujunchang97@outlook.com>
2026-08-05 19:46:29 +0800

    libssh

---
+++ libssh/build.ps1
@@ -0,0 +1,12 @@
+$cmake_options = @(
+  '-DBUILD_SHARED_LIBS=ON',
+  '-DWITH_EXAMPLES=OFF',
+  "-DGSSAPI_ROOT_DIR=$PREFIX\Library",
+  '-DGSSAPI_FLAVOR_MIT=ON'
+  '-DGSSAPI_DEFINITIONS=WIN32_LEAN_AND_MEAN'
+  '-DGSSAPI_LIBRARIES=gssapi64.lib;krb5_64.lib;comerr64.lib'
+)
+
+cmake -S . -B build @cmake_options
+cmake --build build
+cmake --install build
