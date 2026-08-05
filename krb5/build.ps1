d83186a90d9a7894a0a02c2eea5f6c3fcc012d6f
PaperChalice <liujunchang97@outlook.com>
2026-08-05 16:03:37 +0800

    add mos

--- krb5/build.ps1
+++ krb5/build.ps1
@@ -8,3 +8,13 @@ $Env:MY_CFLAGS = $CFLAGS
 nmake -f Makefile.in prep-windows
 nmake NODEBUG=1
 nmake install NODEBUG=1
+
+# Generate mo files
+Set-Location po
+$langs = Get-ChildItem *.po | Select-Object -ExpandProperty BaseName
+foreach($lang in $langs) {
+  msgfmt "${lang}.po" -o "${lang}.mo"
+  $dst = "$LIBRARY_PREFIX\share\locale\$lang\LC_MESSAGES"
+  New-Item -Path "$LIBRARY_PREFIX\share\locale\$lang\LC_MESSAGES" -ItemType Directory -Force
+  Move-Item "${lang}.mo" "$dst\mit-krb5.mo"
+}
