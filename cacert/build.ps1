9909aaa830443f265859e5dad5417a5b5f5584b3
PaperChalice <liujunchang97@outlook.com>
2026-08-06 21:52:15 +0800

    use internal name

--- cacert/build.ps1
+++ cacert/build.ps1
@@ -1,2 +1,2 @@
 New-Item -ItemType Directory $LIBRARY_PREFIX\SSL -Force
-Copy-Item cacert.pem -Destination $LIBRARY_PREFIX\SSL
+Copy-Item cacert.pem -Destination $LIBRARY_PREFIX\SSL\curl-ca-bundle.crt
